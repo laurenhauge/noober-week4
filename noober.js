34b817216e93911d6dd59b3b47e2930fa1164b51
Lauren Hauge <lauren.hauge@kellogg.northwestern.edu>
2021-04-29 09:20:08 -0500

    v1

--- noober.js
+++ noober.js
@@ -6,5 +6,44 @@ window.addEventListener('DOMContentLoaded', async function() {
   console.dir(json)
   
   // 🔥 start here: write the recipe (algorithm), then write the code
-  
+
+  // Create a variable for the rides data
+  let rides = json
+
+  // Loop through the rides data
+  for (let i=0; i < rides.length; i++) {
+  // Create a variable to store each ride in memory
+    let ride = rides[i]
+      
+    let rideFirstName = ride.passengerDetails.first
+    let rideLastName = ride.passengerDetails.last
+    let rideNumber = ride.passengerDetails.phoneNumber
+    let pickupAddress = ride.pickupLocation.address
+    let pickupCity = ride.pickupLocation.city
+    let pickupState = ride.pickupLocation.state
+    let pickupZip = ride.pickupLocation.zip
+    let dropAddress = ride.dropoffLocation.address
+    let dropCity = ride.dropoffLocation.city
+    let dropState = ride.dropoffLocation.state
+    let dropZip = ride.dropoffLocation.zip
+    let numberOfPassengers = ride.numberOfPassengers
+    let purpleRequested = ride.purpleRequested
+
+    let serviceLevel
+
+    if (purpleRequested == true) {
+      serviceLevel = `Noober Purple`
+    } else if (numberOfPassengers >= 3){
+      serviceLevel = `Noober XL`
+    } else {
+      serviceLevel = `Noober X`
+    }
+
+  // console.log(serviceLevel)
+    // Create a variable for the HTML element we're going to add to
+    let ridesElement = document.querySelector(`.rides`)
+      
+    // Insert HTML into the rides element, using the data from each ride
+ 
+  }
 })
